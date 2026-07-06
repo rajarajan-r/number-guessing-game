a9888283f909c2b2cabb0881796a05be593fec63
rajarajan-r <kkdraja695@gmail.com>
2026-07-06 23:31:40 +0530

    Update numberguessing.java

--- numberguessing.java
+++ numberguessing.java
@@ -1,35 +1,28 @@
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
+class NumberGuessingGame{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Random rand=new Random();
 
-class NumberGuessingGame {
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-
-        int numberToGuess = rand.nextInt(100) + 1; // 1 to 100
-        int guess = 0;
-        int attempts = 0;
-
+        int numberToGuess=rand.nextInt(100)+1; 
+        int guess=0;
+        int attempts=0;
         System.out.println("Welcome to Number Guessing Game!");
         System.out.println("Guess a number between 1 and 100");
-
-        while (guess != numberToGuess) {
+        while (guess!=numberToGuess){
             System.out.print("Enter your guess: ");
-            guess = sc.nextInt();
+            guess=sc.nextInt();
             attempts++;
-
-            if (guess > numberToGuess) {
+             if(guess>numberToGuess){
                 System.out.println("Too High! Try again.");
             } 
-            else if (guess < numberToGuess) {
+            else if (guess<numberToGuess){
                 System.out.println("Too Low! Try again.");
             } 
             else {
-                System.out.println("🎉 Correct! You guessed it in " + attempts + " attempts.");
+                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
             }
         }
-
-        sc.close();
+      sc.close();
     }
 }
